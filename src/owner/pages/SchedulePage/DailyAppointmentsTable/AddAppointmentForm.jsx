14ec23786a82a01ce38c7be59a050700b063b3b4
Majd Salameh <majdsalameh9@gmail.com>
2026-02-11 12:05:19 +0200

    11 v4

--- src/owner/pages/SchedulePage/DailyAppointmentsTable/AddAppointmentForm.jsx
+++ src/owner/pages/SchedulePage/DailyAppointmentsTable/AddAppointmentForm.jsx
@@ -179,7 +179,7 @@ const AddAppointmentForm = ({ isOpen, onClose, onSave }) => {
             ביטול
           </button>
           
-          <button type="submit" form="addApptForm" className="btn-save">
+          <button type="submit" form="addApptForm" className="modal-btn-save">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
               <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
